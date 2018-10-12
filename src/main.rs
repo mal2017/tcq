ccd2911f17fabd9f5196916ddbf238ecba5c1def
mattlawlor <matt.a.lawlor@gmail.com>
2018-10-12 17:20:57 -0400

    v0.1.1

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() {
 	let matches = App::new("tcq")
                           .version("0.1.1")
                           .author("Matt Lawlor <matt.a.lawlor@gmail.com>")
-                          .about("T>>C conversion annotation util for working with nascent RNA species.\nAdds valid T>>C conversions to tag of your choice.\nRequires revcomp (-) seqs & MD tags.")
+                          .about("Util for SLAM-/Timelapse-seq. Adds valid T>>C conversions to tag of your choice.\nRequires revcomp (-) seqs & MD tags.")
                           .arg(Arg::with_name("IBAM")
                                .help("bam to annotate")
                                .required(true)
