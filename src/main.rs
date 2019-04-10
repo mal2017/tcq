a629ada7feb28872f07d28051b7ed3b75173a547
Matthew Lawlor <matt.a.lawlor@gmail.com>
2019-04-10 09:58:32 -0400

    bump version

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() {
     use tcq::runner;
 
     let matches = App::new("tcq")
-                          .version("0.2.0")
+                          .version("0.4.0")
                           .author("Matt Lawlor <matt.a.lawlor@gmail.com>")
                           .about("Util for SLAM-/Timelapse-seq. Adds valid T>>C conversions to tag of your choice.\nRequires revcomp (-) seqs & MD tags.")
                           .arg(Arg::with_name("IBAM")
